c29cef5a87fd38614fae5a962157c00a633a9a53
jenkins <jenkins@gmail.com>
2018-01-27 07:09:29 +0000

    this is jagan project

---
+++ src/main/java/jagan/mohan/reddy/App.java
@@ -0,0 +1,13 @@
+package jagan.mohan.reddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
